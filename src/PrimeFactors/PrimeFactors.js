976b538fa3dfbff559a157d94061f9b97cab6b06
Paul Sobocinski <psobocinski@connected.io>
2019-05-17 15:00:33 -0400

    web test club May 17

--- src/PrimeFactors/PrimeFactors.js
+++ src/PrimeFactors/PrimeFactors.js
@@ -1,4 +1,13 @@
 function generate(number) {
+  if (number === 1) return [];
+
+  if (number > 2 && number % 2 === 0) { // even
+    return [2].concat(generate(number / 2));
+  } else if (number > 2 && number % 3 === 0) {
+    return [3].concat(generate(number / 3));
+  }
+
+  return [number];
 }
 
 module.exports = {
--- src/PrimeFactors/PrimeFactors.test.js
+++ src/PrimeFactors/PrimeFactors.test.js
@@ -1,6 +1,50 @@
 const PrimeFactors = require('./PrimeFactors');
 
 describe('PrimeFactors', () => {
+
+      // * 4620 should return `[2,2,3,5,7,11]`
+
+
+  // * 1 should return `[]`
+  describe('#generate ', () => {
+    it('should return [] when given 1', () => {
+      expect(PrimeFactors.generate(1)).toEqual([]);
+    });
+
+    it('should return [2] when given 2', () => {
+      expect(PrimeFactors.generate(2)).toEqual([2]);
+    });
+
+    it('3 should return `[3]`', () => {
+      expect(PrimeFactors.generate(3)).toEqual([3]);
+    });
+
+    it('4 should return `[2, 2]`', () => {
+      expect(PrimeFactors.generate(4)).toEqual([2, 2]);
+    });
+
+    it('5 should return `[5]`', () => {
+      expect(PrimeFactors.generate(5)).toEqual([5]);
+    });
+
+    it('6 should return `[2, 3]`', () => {
+      expect(PrimeFactors.generate(6)).toEqual([2, 3]);
+    });
+
+    it('7 should return `[7]`', () => {
+      expect(PrimeFactors.generate(7)).toEqual([7]);
+    });
+
+    it('8 should return `[2, 2, 2]`', () => {
+      expect(PrimeFactors.generate(8)).toEqual([2, 2, 2]);
+    });
+
+    it('9 should return `[3, 3]`', () => {
+      expect(PrimeFactors.generate(9)).toEqual([3, 3]);
+    });
+  });
+
+
   it('works', () => {
     expect(true).toBe(true);
   });
