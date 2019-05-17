bf4702453fc0e6fdb4cfe98b3e253109c40d7922
Paul Sobocinski <psobocinski@connected.io>
2019-05-17 10:14:22 -0400

    Remove solutions

--- src/FizzBuzz/FizzBuzz.js
+++ src/FizzBuzz/FizzBuzz.js
@@ -1,37 +1,4 @@
-const _ = require('lodash');
-
-const DEFAULT_RULES = {
-  3: 'Fizz',
-  5: 'Buzz'
-};
-
 class FizzBuzz {
-  constructor(rules = {}) {
-    this.rules = { ...DEFAULT_RULES, ...rules };
-  }
-
-  fizzBuzz(n) {
-    let result = '';
-
-    _.forEach(this.rules, (word, num) => {
-      if (n % num === 0) {
-        result += word;
-      }
-    });
-    if (result) {
-      return result;
-    }
-
-    return String(n);
-  }
-
-  generate(range = { from: 1, to: 15 }) {
-    const result = [];
-    for (let i = range.from; i <= range.to; i++) {
-      result.push(this.fizzBuzz(i));
-    }
-    return result;
-  }
 }
 
-module.exports = { FizzBuzz };
+module.exports = FizzBuzz;
--- src/PrimeFactors/PrimeFactors.js
+++ src/PrimeFactors/PrimeFactors.js
@@ -1,17 +1,4 @@
-/**
- * @param number {int}
- * @returns {Array<int>} prime factors
- */
 function generate(number) {
-  const factors = [];
-
-  for (let divisor = 2; number > 1; divisor++) {
-    for (; number % divisor === 0; number /= divisor) {
-      factors.push(divisor);
-    }
-  }
-
-  return factors;
 }
 
 module.exports = {
