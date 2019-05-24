654f941e6f796d4988a94db3357bd171255921d2
Paul Sobocinski <psobocinski@connected.io>
2019-05-24 15:01:17 -0400

    web test club may 24

--- src/PrimeFactors/PrimeFactors.js
+++ src/PrimeFactors/PrimeFactors.js
@@ -1,13 +1,56 @@
 function generate(number) {
   if (number === 1) return [];
 
-  if (number > 2 && number % 2 === 0) { // even
-    return [2].concat(generate(number / 2));
-  } else if (number > 2 && number % 3 === 0) {
-    return [3].concat(generate(number / 3));
-  }
+  // TODO: Implement Sieve
+  const PRIMES = _generatePrimes(number);
 
-  return [number];
+  let primeFactors = [];
+  console.log("primes for number:", number, PRIMES);
+  // const candidatePrime = PRIMES.find((prime) => (number % prime) === 0);
+
+  let remainder = number;
+  PRIMES.forEach(( prime )=> {
+    while (remainder % prime === 0) {
+      primeFactors.push(prime);
+      remainder /= prime ;
+    }
+  });
+
+
+  return primeFactors;
+}
+
+/*
+ Input: an integer n > 1.
+
+ Let A be an array of Boolean values, indexed by integers 2 to n,
+ initially all set to true.
+
+ for i = 2, 3, 4, ..., not exceeding √n:
+   if A[i] is true:
+     for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n:
+       A[j] := false.
+
+ Output: all i such that A[i] is true.
+* */
+
+function _generatePrimes(number) {
+  if (number === 1) return [];
+  const primeArray = [true, true];
+  let result = [];
+  for (let i = 2; i < Math.floor(Math.sqrt(number)); i++) {
+    if(!primeArray[i]) {
+      for(let j = i*i; j <= number; j+=i) {
+        primeArray[j] = true;
+      }
+    }
+  }
+  for(let i = 2; i <= primeArray.length; i++) {
+    if(!primeArray[i]) {
+      result.push(i);
+    }
+  }
+  return result;
 }
 
 module.exports = {
--- src/PrimeFactors/PrimeFactors.test.js
+++ src/PrimeFactors/PrimeFactors.test.js
@@ -42,6 +42,14 @@ describe('PrimeFactors', () => {
     it('9 should return `[3, 3]`', () => {
       expect(PrimeFactors.generate(9)).toEqual([3, 3]);
     });
+
+    it( '4620 should return `[2,2,3,5,7,11]`', () => {
+      expect(PrimeFactors.generate(4620)).toEqual([2,2,3,5,7,11]);
+    });
+
+    it( '676 should return `[2, 2, 13, 13]`', () => {
+      expect(PrimeFactors.generate(676)).toEqual([2,2,13, 13]);
+    })
   });
 
 
