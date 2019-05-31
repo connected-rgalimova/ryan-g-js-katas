4f8113b856b4066ca27ca6bbd0181689d9d83c43
Paul Sobocinski <psobocinski@connected.io>
2019-05-31 15:01:21 -0400

    web test club may 31

--- src/StringCalculator/StringCalculator.js
+++ src/StringCalculator/StringCalculator.js
@@ -1,4 +1,27 @@
 class StringCalculator {
+  add(input) {
+    // TODO split by \n and grab the first element
+    // check if it starts with //
+    // if yes set the value after // as delimiter
+    const customDelimiter = input.split('\n');
+
+    const splitString = input.split(/[,\n]/);
+    let sum = 0;
+    let negatives = [];
+    for (let i = 0; i < splitString.length; i++) {
+      let num = Number(splitString[i]);
+      if (num < 0) {
+        negatives.push(num)
+      } else if (num > 1000) {
+        continue
+      }
+      sum += num;
+    }
+    if (negatives.length > 0) {
+      throw new Error('negatives not allowed: ' + negatives.join(','))
+    }
+    return sum;
+  }
 }
 
 module.exports = StringCalculator;
--- src/StringCalculator/StringCalculator.test.js
+++ src/StringCalculator/StringCalculator.test.js
@@ -1,7 +1,52 @@
 const StringCalculator = require('./StringCalculator');
 
+// Write a method `add` under an object `StringCalculator` that, given a delimited string, returns the sum of the numbers in the string.
+//
+// 1. An empty string returns zero `'' => 0`
+// 2. A single number returns the value `'1' => 1` `'2' => 2`
+// 3. Two numbers, comma delimited, returns the sum `'1,2' => 3` `'10,20' => 30`
+// 4. Two numbers, newline delimited, returns the sum `'1\n2' => 3`
+// 5. Three numbers, delimited either way, returns the sum `'1\n2,3\n4' => 10`
+// 6. Negative numbers throw an exception with the message `'-1,2,-3' => 'negatives not allowed: -1,-3'`
+//
+// **stop here if you are a beginner. Continue if you can finish the steps so far in less than 30 minutes.**
+//
+// 7. Numbers greater than 1000 are ignored
+// 8. A single char delimiter can be defined on the first line starting with `//` (e.g `//#\n1#2` for a ‘#’ as the delimiter)
+// 9. A multi char delimiter can be defined on the first line starting with `//` (e.g. `//###\n1###2` for ‘###’ as the delimiter)
+
 describe('StringCalculator', () => {
-  it('works', () => {
-    expect(true).toBe(true);
+  const add = (new StringCalculator()).add;
+
+  it('should return 0 when given an empty string', () => {
+    expect(add('')).toBe(0);
+  });
+
+  it('should return the number when given a single number as a string', () => {
+    expect(add('1')).toBe(1);
+  });
+
+  it('should return the sum when given two comma delimited numbers', () => {
+    expect(add('1,2')).toBe(3)
+  });
+
+  it('should return the sum when given two newline delimited numbers', () => {
+    expect(add(`3\n4`)).toBe(7);
+  });
+
+  it('should return the sum when given more then two numbers mixed deliminators', () => {
+    expect(add(`1\n2,3\n4`)).toBe(10);
+  });
+
+  it('should throw an exception for negative numbers', () => {
+    expect(() => add(`-1,2,-3`)).toThrow(new Error('negatives not allowed: -1,-3'));
+  });
+
+  it('Numbers greater than 1000 are ignored', () => {
+    expect(add(`1001,1`)).toBe(1);
+  });
+
+  it('A single char delimiter can be defined on the first line starting with `//`', () => {
+    expect(add(`//#\n1#2#3`)).toBe(6);
   });
 });
