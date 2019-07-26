ba70c351b834d8db3065e5cd441ff4fb0347d7a2
Paul Sobocinski <psobocinski@connected.io>
2019-07-26 15:09:18 -0400

    web test club july 26

--- src/RomanNumerals/RomanNumerals.js
+++ src/RomanNumerals/RomanNumerals.js
@@ -1,4 +1,39 @@
+const letterToInteger = {
+  "I": 1,
+  "V": 5,
+  "L": 50,
+  "X": 10,
+  "C": 100,
+  "D": 500,
+  "M": 1000,
+};
+
 class RomanNumerals {
+
+  static toInteger(letters) {
+
+    let number = 0;
+    for (let i = 0; i < letters.length; i += 1) {
+      let letter = letters.charAt(i);
+      if (letter === "I") {
+        number = this.calculateNumber(i, letters, ["V", "X"], number, letter);
+      } else if (letter === "X") {
+        number = this.calculateNumber(i, letters, ["L", "C"], number, letter);
+      } else {
+        number += letterToInteger[letter];
+      }
+    }
+    return number;
+  }
+
+  static calculateNumber(i, letters, subtractiveLetters, number, letter) {
+    if ((i + 1 <= letters.length) && subtractiveLetters.includes(letters.charAt(i + 1))) {
+      number -= letterToInteger[letter];
+    } else {
+      number += letterToInteger[letter];
+    }
+    return number;
+  }
 }
 
 module.exports = RomanNumerals;
--- src/RomanNumerals/RomanNumerals.test.js
+++ src/RomanNumerals/RomanNumerals.test.js
@@ -5,7 +5,44 @@ describe('RomanNumerals', () => {
   // 1. Given an Integer, return as a Roman Numeral
   // 1. Given a Roman Numeral, return as an Integer
 
-  it('works', () => {
-    expect(true).toBe(true);
+  it('converts I to 1', () => {
+    expect(RomanNumerals.toInteger('I')).toEqual(1);
+  });
+
+  it('converts V to 5', () => {
+    expect(RomanNumerals.toInteger('V')).toEqual(5);
+  });
+  it('converts X to 10', () => {
+    expect(RomanNumerals.toInteger('X')).toEqual(10);
+  });
+  it('converts L to 50', () => {
+    expect(RomanNumerals.toInteger('L')).toEqual(50);
+  });
+  it('converts C to 100', () => {
+    expect(RomanNumerals.toInteger('C')).toEqual(100);
+  });
+  it('converts D to 500', () => {
+    expect(RomanNumerals.toInteger('D')).toEqual(500);
+  });
+  it('converts M to 1000', () => {
+    expect(RomanNumerals.toInteger('M')).toEqual(1000);
+  });
+  it('converts II to 2', () => {
+    expect(RomanNumerals.toInteger('II')).toEqual(2);
+  });
+  it('converts IV to 4', () => {
+    expect(RomanNumerals.toInteger('IV')).toEqual(4);
+  });
+  it('converts IX to 9', () => {
+    expect(RomanNumerals.toInteger('IX')).toEqual(9);
+  });
+  it('converts XL to 40', () => {
+    expect(RomanNumerals.toInteger('XL')).toEqual(40);
+  });
+  it('converts XC to 90', () => {
+    expect(RomanNumerals.toInteger('XC')).toEqual(90);
+  });
+  it('converts CD to 400', () => {
+    expect(RomanNumerals.toInteger('CD')).toEqual(400);
   });
 });
