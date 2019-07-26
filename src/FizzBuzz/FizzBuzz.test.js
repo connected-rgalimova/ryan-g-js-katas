d6f7d19d5a90ebeb8e0ed5d1518aa8fa7bd18df7
Paul Sobocinski <psobocinski@connected.io>
2019-07-26 12:43:31 -0400

    Add instructions for FizzBuzz

--- src/FizzBuzz/FizzBuzz.test.js
+++ src/FizzBuzz/FizzBuzz.test.js
@@ -1,6 +1,12 @@
 const FizzBuzz = require('./FizzBuzz');
 
 describe(`FizzBuzz`, () => {
+
+  // "Write a program that prints the numbers from 1 to 100.
+  // But for multiples of three print “Fizz” instead of the number
+  // and for the multiples of five print “Buzz”.
+  // For numbers which are multiples of both three and five print “FizzBuzz”."
+
   it('works', () => {
     expect(true).toBe(true);
   });
