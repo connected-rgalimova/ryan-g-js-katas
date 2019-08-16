b815dbf809199f84acfcdcfa54c4999681e64d3e
Paul Sobocinski <psobocinski@connected.io>
2019-08-16 15:13:14 -0400

    web test club august 16

--- src/RomanNumerals/RomanNumerals.js
+++ src/RomanNumerals/RomanNumerals.js
@@ -8,6 +8,22 @@ const letterToInteger = {
   "M": 1000,
 };
 
+const subtractiveLetters = {
+  "I": ["V", "X"],
+  "X": ["L", "C"],
+  "C": ["D"],
+};
+
+const invalidNextLetters = {
+  "I": ["L", "C", "D", "M"],
+  "V": ["V", "L", "X", "C", "D", "M"],
+  "L": ["C", "D", "M"],
+  "X": ["D", "M"],
+  "C": [],
+  "D": ["D", "M"],
+  "M": [],
+};
+
 class RomanNumerals {
 
   static toInteger(letters) {
@@ -15,19 +31,23 @@ class RomanNumerals {
     let number = 0;
     for (let i = 0; i < letters.length; i += 1) {
       let letter = letters.charAt(i);
-      if (letter === "I") {
-        number = this.calculateNumber(i, letters, ["V", "X"], number, letter);
-      } else if (letter === "X") {
-        number = this.calculateNumber(i, letters, ["L", "C"], number, letter);
-      } else {
-        number += letterToInteger[letter];
-      }
+      number = this.calculateNumber(i, letters, number, letter);
     }
     return number;
   }
 
-  static calculateNumber(i, letters, subtractiveLetters, number, letter) {
-    if ((i + 1 <= letters.length) && subtractiveLetters.includes(letters.charAt(i + 1))) {
+  static calculateNumber(i, letters, number, letter) {
+    // TODO: WIP refactor for readability
+    if (i + 1 <= letters.length && letterToInteger[letters.charAt(i)] < letterToInteger[letters.charAt(i + 1)]) {
+      if (!subtractiveLetters[letter] || !subtractiveLetters[letter].includes(letters.charAt(i + 1))) {
+        throw new Error(`${letters} is not a valid Roman Numeral`)
+      }
+      // const nextLetter = letters[i + 1];
+      // if (invalidNextLetters[letter].includes(nextLetter)) {
+      //   throw new Error(`${letter}${nextLetter} is not a valid Roman Numeral`)
+      // }
+    }
+    if ((i + 1 <= letters.length) && subtractiveLetters[letter] && subtractiveLetters[letter].includes(letters.charAt(i + 1))) {
       number -= letterToInteger[letter];
     } else {
       number += letterToInteger[letter];
--- src/RomanNumerals/RomanNumerals.test.js
+++ src/RomanNumerals/RomanNumerals.test.js
@@ -5,44 +5,51 @@ describe('RomanNumerals', () => {
   // 1. Given an Integer, return as a Roman Numeral
   // 1. Given a Roman Numeral, return as an Integer
 
-  it('converts I to 1', () => {
-    expect(RomanNumerals.toInteger('I')).toEqual(1);
-  });
+  const toI = letter => RomanNumerals.toInteger(letter)
 
+  it('converts I to 1', () => {
+    expect(toI('I')).toEqual(1);
+  });
   it('converts V to 5', () => {
-    expect(RomanNumerals.toInteger('V')).toEqual(5);
+    expect(toI('V')).toEqual(5);
   });
   it('converts X to 10', () => {
-    expect(RomanNumerals.toInteger('X')).toEqual(10);
+    expect(toI('X')).toEqual(10);
   });
   it('converts L to 50', () => {
-    expect(RomanNumerals.toInteger('L')).toEqual(50);
+    expect(toI('L')).toEqual(50);
   });
   it('converts C to 100', () => {
-    expect(RomanNumerals.toInteger('C')).toEqual(100);
+    expect(toI('C')).toEqual(100);
   });
   it('converts D to 500', () => {
-    expect(RomanNumerals.toInteger('D')).toEqual(500);
+    expect(toI('D')).toEqual(500);
   });
   it('converts M to 1000', () => {
-    expect(RomanNumerals.toInteger('M')).toEqual(1000);
+    expect(toI('M')).toEqual(1000);
   });
   it('converts II to 2', () => {
-    expect(RomanNumerals.toInteger('II')).toEqual(2);
+    expect(toI('II')).toEqual(2);
   });
   it('converts IV to 4', () => {
-    expect(RomanNumerals.toInteger('IV')).toEqual(4);
+    expect(toI('IV')).toEqual(4);
   });
   it('converts IX to 9', () => {
-    expect(RomanNumerals.toInteger('IX')).toEqual(9);
+    expect(toI('IX')).toEqual(9);
   });
   it('converts XL to 40', () => {
-    expect(RomanNumerals.toInteger('XL')).toEqual(40);
+    expect(toI('XL')).toEqual(40);
   });
   it('converts XC to 90', () => {
-    expect(RomanNumerals.toInteger('XC')).toEqual(90);
+    expect(toI('XC')).toEqual(90);
   });
   it('converts CD to 400', () => {
-    expect(RomanNumerals.toInteger('CD')).toEqual(400);
+    expect(toI('CD')).toEqual(400);
+  });
+
+  describe('validating input', () => {
+    it('throws error when input is an invalid roman numeral combination', () => {
+      expect(() => toI('IL')).toThrowError('IL is not a valid Roman Numeral');
+    });
   });
 });
