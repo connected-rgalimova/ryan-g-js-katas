79b19cc626e3674afc4801e31e2defeb24b70bff
Paul Sobocinski <psobocinski@connected.io>
2019-10-25 13:50:29 -0400

    WIP

--- src/StringCalculator/StringCalculator.js
+++ src/StringCalculator/StringCalculator.js
@@ -1,4 +1,25 @@
 class StringCalculator {
+    static add(str) {
+        if (str === '') {
+            return 0;
+        }
+        if(str.match(/,|\n/g))
+        if (str.includes(',')) {
+            return StringCalculator.sumWithDelimiter(str, ',');
+        }
+        if (str.includes("\n")) {
+            return StringCalculator.sumWithDelimiter(str, "\n");
+        };
+        return Number(str);
+    }
+
+    static sumWithDelimiter(str, newLocal_1) {
+        const str_arr = str.split(newLocal_1);
+        return str_arr.reduce((init, ele) => {
+            init += Number(ele);
+            return init;
+        }, 0);
+    }
 }
 
 module.exports = StringCalculator;
--- src/StringCalculator/StringCalculator.test.js
+++ src/StringCalculator/StringCalculator.test.js
@@ -1,7 +1,37 @@
 const StringCalculator = require('./StringCalculator');
 
+// Write a method `add` under an object `StringCalculator` that, given a delimited string, returns the sum of the numbers in the string.
+
 describe('StringCalculator', () => {
   it('works', () => {
     expect(true).toBe(true);
+  });
+
+  describe('.add()', () => {
+  // 1. An empty string returns zero `'' => 0`
+    it('returns zero when empty string is given', () => {
+      const result = StringCalculator.add('');
+
+      expect(result).toEqual(0);
+    });
+
+    // 2. A single number returns the value `'1' => 1` `'2' => 2`
+    it('returns the value if it is a single number string', () => {
+      const result = StringCalculator.add('1');
+      expect(result).toEqual(1);
+    });
+
+    // 3. Two numbers, comma delimited, returns the sum `'1,2' => 3` `'10,20' => 30`
+    it('returns the sum when string contains two numbers, comma delimited', () => {
+      const result = StringCalculator.add('1,2');
+      expect(result).toEqual(3);
+    });
+
+    
+    // 4. Two numbers, newline delimited, returns the sum `'1\n2' => 3`
+    it('returns the sum when string contains two numbers, newline delimited', () => {
+      const result = StringCalculator.add('1\n2');
+      expect(result).toEqual(3);
+    });
   });
 });
