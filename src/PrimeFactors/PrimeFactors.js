3421878c5fde9d860817d065913966354366134e
connected-psobocinski <psobocinski@connected.io>
2021-11-03 11:06:51 -0400

    prime factors TDD

--- src/PrimeFactors/PrimeFactors.js
+++ src/PrimeFactors/PrimeFactors.js
@@ -1,5 +1,25 @@
-function generate(number) {}
+function generate(number) {
+  const primes = [2, 3, 5, 7, 11];
+  const result = [];
 
-module.exports = {
-  generate
-};
+  // loop through all numbers y smaller than number x (stopping at 1)
+  // for each iteration, check if x / y has a remainder of zero
+  // if it does:
+  //  - push y into the result array
+  //  - reassign x as x / y (integer division)
+
+  primes.forEach(prime => {
+    while (number % prime === 0) {
+      result.push(prime);
+      number = number / prime;
+    }      
+  });
+
+  if (primes.includes(number)) {
+    result.push(number)
+  }
+
+  return result;
+}
+
+module.exports = generate;
--- src/PrimeFactors/PrimeFactors.test.js
+++ src/PrimeFactors/PrimeFactors.test.js
@@ -1,7 +1,53 @@
 const PrimeFactors = require('./PrimeFactors');
 
 describe('PrimeFactors', () => {
-  it('works', () => {
-    expect(true).toBe(true);
+  it('1 should return []', () => {
+    const result = PrimeFactors(1);
+    expect(result).toEqual([]);
+  });
+
+  it('2 should return [2]', () => {
+    const result = PrimeFactors(2);
+    expect(result).toEqual([2]);
+  });
+
+  it('3 should return [3]', () => {
+    const result = PrimeFactors(3);
+    expect(result).toEqual([3]);
+  });
+
+  it('4 should return [2,2]', () => {
+    const result = PrimeFactors(4);
+    expect(result).toEqual([2, 2]);
+  });
+
+  it('5 should return [5]', () => {
+    const result = PrimeFactors(5);
+    expect(result).toEqual([5]);
+  });
+
+  it('6 should return [2,3]', () => {
+    const result = PrimeFactors(6);
+    expect(result).toEqual([2, 3]);
+  });
+
+  it('7 should return [7]', () => {
+    const result = PrimeFactors(7);
+    expect(result).toEqual([7]);
+  });
+
+  it('8 should return [2,2,2]', () => {
+    const result = PrimeFactors(8);
+    expect(result).toEqual([2, 2, 2]);
+  });
+
+  it('9 should return [3,3]', () => {
+    const result = PrimeFactors(9);
+    expect(result).toEqual([3, 3]);
+  });
+
+  it('4620 should return [2,2,3,5,7,11]', () => {
+    const result = PrimeFactors(4620);
+    expect(result).toEqual([2, 2, 3, 5, 7, 11]);
   });
 });
