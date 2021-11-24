99d143b63b79bc7f563925f016f61620285f740d
Rachel Wong <rwong@connected.io>
2021-11-24 14:35:30 -0500

    able to get the day with minimum temp spread

--- src/DataMunging/DataMunging.js
+++ src/DataMunging/DataMunging.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const DataMunging = (path) => {
     const array = fs.readFileSync(path).toString().split("\n");
-    array.splice(0,1);
+    array.splice(0,2);
     const returnArray = [];
     for (const item of array) {
         splittedItem = item.split(" ").filter(cell => cell !== "");
@@ -14,4 +14,17 @@ const DataMunging = (path) => {
     return returnArray;
 }
 
-module.exports = DataMunging;
+const dayWithSmallestTempSpread = (data) => {
+    let minTempSpread_idx = null;
+    let minTempSpread = null;
+    for (let i = 0; i < data.length; i++) {
+        if (minTempSpread_idx === null || data[i][0]-data[i][1] < minTempSpread) {
+            minTempSpread_idx = i;
+            minTempSpread = data[i][0]-data[i][1];
+        }
+    }
+
+    return minTempSpread_idx + 1;
+}
+
+module.exports = {DataMunging, dayWithSmallestTempSpread};
--- src/DataMunging/DataMunging.test.js
+++ src/DataMunging/DataMunging.test.js
@@ -1,4 +1,4 @@
-const DataMunging = require('./DataMunging');
+const {DataMunging, dayWithSmallestTempSpread} = require('./DataMunging');
 
 describe('Data Munging', () => {
     it('should parse the weather.dat into an array with objects', () => {
@@ -7,8 +7,26 @@ describe('Data Munging', () => {
         // 2   3    20
 
         const arr = DataMunging('src/DataMunging/test.dat')
-        const row1 = [2, 10]
-        const row2 = [3, 20]
+        const row1 = [88, 59]
+        const row2 = [79, 63]
         expect(arr).toEqual([row1, row2])
     })
+
+    // write a program to output the day number (column one) 
+    // with the smallest temperature spread
+    // (the maximum temperature is the second column, the minimum the third column).
 })
+
+describe('dayWithSmallestTempSpread', () => {
+    it('should return day  number (column one) with smallest temperature spread', () => {
+        const weatherData = [
+            [100, 50], // day 1
+            [55, 50], // day 2, smallest spread
+            [75, 50], // day 3
+        ];
+
+        const result = dayWithSmallestTempSpread(weatherData);
+
+        expect(result).toEqual(2);
+    });
+});
