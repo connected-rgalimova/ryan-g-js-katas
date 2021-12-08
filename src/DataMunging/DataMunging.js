7d24b7a875d4949106d66fbf9573509728d735b9
Rachel Wong <rwong@connected.io>
2021-12-08 14:53:21 -0500

    finished part 1 and moved to part 2, refactor wip

--- src/DataMunging/DataMunging.js
+++ src/DataMunging/DataMunging.js
@@ -19,14 +19,41 @@ const dayWithSmallestTempSpread = (data) => {
     let minTempSpread = [];
     for (let i = 0; i < data.length; i++) {
         const curDayTempSpread = data[i][0] - data[i][1];
-        console.log(curDayTempSpread);
-        if (minTempSpread_idx.length === 0 || minTempSpread.length === 0 || curDayTempSpread < minTempSpread[0]) {
+        if (minTempSpread_idx.length === 0 || minTempSpread.length === 0 || curDayTempSpread === minTempSpread[0]) {
             minTempSpread_idx.push(i);
             minTempSpread.push(curDayTempSpread);
+        } else if (curDayTempSpread < minTempSpread[0]) {
+            minTempSpread_idx = [i];
+            minTempSpread = [curDayTempSpread];
         }
+    }
+
+    if (minTempSpread_idx.length > 1) {
+        throw 'there is more than 1 day has the smallest spread';
     }
 
     return minTempSpread_idx[0] + 1;
 }
 
-module.exports = {DataMunging, dayWithSmallestTempSpread};
+const teamWithSmallestGoalSpread = (data) => {
+    let minGoalSpread_name = [];
+    let minGoalSpread = [];
+    for (let i = 0; i < data.length; i++) {
+        const curGoalSpead = data[i][5] - data[i][7];
+        if (minGoalSpread_name.length === 0 || minGoalSpread.length === 0 || curGoalSpead === minGoalSpread[0]) {
+            minGoalSpread_name.push(data[i][0]);
+            minGoalSpread.push(curGoalSpead);
+        } else if (curGoalSpead < minGoalSpread[0]) {
+            minGoalSpread_name = [data[i][0]];
+            minGoalSpread = [curGoalSpead];
+        }
+    }
+
+    if (minGoalSpread_name.length > 1) {
+        throw 'there is more than 1 day has the smallest spread';
+    }
+
+    return minGoalSpread_name[0];
+} 
+
+module.exports = {DataMunging, dayWithSmallestTempSpread, teamWithSmallestGoalSpread};
--- src/DataMunging/DataMunging.test.js
+++ src/DataMunging/DataMunging.test.js
@@ -1,4 +1,4 @@
-const {DataMunging, dayWithSmallestTempSpread} = require('./DataMunging');
+const {DataMunging, dayWithSmallestTempSpread, teamWithSmallestGoalSpread} = require('./DataMunging');
 
 describe('Data Munging', () => {
     it('should parse the weather.dat into an array with objects', () => {
@@ -16,9 +16,31 @@ describe('Data Munging', () => {
     // with the smallest temperature spread
     // (the maximum temperature is the second column, the minimum the third column).
 })
+/*
+Part Two: Soccer League Table
+The file football.dat contains the results from the English Premier League for 2001/2. 
+The columns labeled ‘F’ and ‘A’ contain the total number of goals scored for and against each team 
+in that season (so Arsenal scored 79 goals against opponents, and had 36 goals scored against them). 
+Write a program to print the name of the team with the smallest difference in ‘for’ and ‘against’ 
+goals.
+*/
+
+describe('teamWithSmallestGoalSpread', () => { 
+    it('should return the name of the team with the smallest difference in ‘for’ and ‘against’ goals', () => {
+        const teamData = [
+            ['Arsenal', '38', '26', '9', '3', '79', '-', '36', '87'],
+            ['Liverpool', '38', '24', '8', '6', '67', '-', '30', '80'],
+            ['Manchester_U', '38', '24', '5', '9', '87', '-', '45', '77'],
+        ];
+
+        const result = teamWithSmallestGoalSpread(teamData);
+
+        expect(result).toEqual('Liverpool');
+    })
+});
 
 describe('dayWithSmallestTempSpread', () => {
-    it.only('should return day  number (column one) with smallest temperature spread', () => {
+    it('should return day  number (column one) with smallest temperature spread', () => {
         const weatherData = [
             [100, 50], // day 1
             [55, 50], // day 2, smallest spread
@@ -29,7 +51,7 @@ describe('dayWithSmallestTempSpread', () => {
 
         expect(result).toEqual(2);
     });
-    it.skip('should return error if there is more than 1 day that has the same temp spread', () => {
+    it('should return error if there is more than 1 day that has the same temp spread', () => {
         const weatherData = [
             [100, 50], // day 1
             [55, 50], // day 2, smallest spread
@@ -37,8 +59,8 @@ describe('dayWithSmallestTempSpread', () => {
             [60, 55], // day 4, smallest spread
         ];
 
-        const result = dayWithSmallestTempSpread(weatherData);
+        // const result = dayWithSmallestTempSpread(weatherData);
 
-        expect(result).toThrowError('there is more than 1 day has the smallest spread');
+        expect(() => dayWithSmallestTempSpread(weatherData)).toThrowError('there is more than 1 day has the smallest spread');
     });
 });
