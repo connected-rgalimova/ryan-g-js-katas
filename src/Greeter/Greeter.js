5cdf464930e108f3bf5bb19af4cd321eda15ad1d
Ryan Galimova <rgalimova@connected.io>
2022-02-24 12:51:16 -0500

    Greeter

--- src/Greeter/Greeter.js
+++ src/Greeter/Greeter.js
@@ -1,4 +1,50 @@
-class Greeter {
+const capitalize = (s) => {
+  return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
-module.exports = Greeter;
+const trace = (s) => {
+  console.log(s)
+  return s
+}
+
+const pipe = (...args) => {
+  return (x) => args.reduce((y, func) => func(y), x)
+}
+
+const getGreeting = (name) => (date) => {
+  const getTimedGreeting = (date) => {
+    if (date) {
+      const morningStart = new Date()
+      morningStart.setHours(6)
+
+      const morningEnd = new Date()
+      morningEnd.setHours(12)
+
+      const eveningStart = new Date()
+      eveningStart.setHours(18)
+
+      const eveningEnd = new Date()
+      eveningEnd.setHours(22)
+
+      if (date > morningStart && date < morningEnd) {
+        return "Good Morning"
+      }
+      if (date > eveningStart && date < eveningEnd) {
+        return "Good Evening"
+      }
+      if (date < morningStart || date > eveningEnd) {
+        return "Good Night"
+      }
+    }
+    return "Hello"
+  }
+  
+  return getTimedGreeting(date) + " " + capitalize(name.trim())
+}
+
+
+function greet(name, date = null) {
+  return pipe(getGreeting(name), trace)(date)
+}
+
+module.exports = { greet };
--- src/Greeter/Greeter.test.js
+++ src/Greeter/Greeter.test.js
@@ -1,7 +1,37 @@
-const Greeter = require('./Greeter');
+const { greet } = require('./Greeter');
 
 describe('Greeter', () => {
-  it('works', () => {
-    expect(true).toBe(true);
+  it('greets with Hello', () => {
+    expect(greet("Bob")).toBe("Hello Bob");
   });
+  it('trimms the input', () => {
+    expect(greet("   Frank  ")).toBe("Hello Frank")
+  })
+  it('capitalizes the name', () => {
+    expect(greet("frank")).toBe("Hello Frank")
+  })
+
+  it("greets in the morning", () => {
+    const today = new Date()
+    today.setHours(8,30)
+    expect(greet("Bob", today)).toBe("Good Morning Bob")
+  }) 
+
+  it("greets in the evening", () => {
+    const today = new Date()
+    today.setHours(19,30)
+    expect(greet("Bob", today)).toBe("Good Evening Bob")
+  }) 
+
+   it("greets at night", () => {
+    const today = new Date()
+    today.setHours(23,30)
+    expect(greet("Bob", today)).toBe("Good Night Bob")
+   }) 
+  
+  it("logs in console each time its called", () => {
+    const logSpy = jest.spyOn(console, 'log')
+    console.log(greet("Bob"))
+    expect(logSpy).toHaveBeenCalledWith("Hello Bob")
+   })
 });
